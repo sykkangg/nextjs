d7d19244d338b2ac97577d168c1508f6ae203eb9
sykkangg <ksy1354@gmail.com>
2025-09-03 14:29:16 +0900

    수정: 리드미 파일 텍스트, Vercel 배포 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: '/nextjs',
-  assetPrefix: '/nextjs',
   images: {
     unoptimized: true
   }
